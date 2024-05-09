0130f0273f72ea27d717bab559b15172cf70b076
steled <steled@gmx.net>
2024-05-09 06:50:43 +0200

    update for kubectl_provider

--- metallb/config.tf
+++ metallb/config.tf
@@ -1,24 +1,21 @@
 resource "kubectl_manifest" "metallb_ipaddresspool" {
   yaml_body = <<YAML
-apiVersion = "metallb.io/v1beta1"
-kind = "IPAddressPool"
-metadata = {
-  name = "generic-cluster-pool"
-  namespace = kubernetes_namespace.metallb.metadata[0].name
-}
-spec = {
-  addresses = [ var.helm_values_addresses ]
-}
-  YAML
+apiVersion: metallb.io/v1beta1
+kind: IPAddressPool
+metadata:
+  name: generic-cluster-pool
+  namespace: ${kubernetes_namespace.metallb.metadata[0].name}
+spec:
+  addresses: ${[ var.helm_values_addresses ]}
+YAML
 }
 
 resource "kubectl_manifest" "metallb_l2advertisement" {
   yaml_body = <<YAML
-apiVersion = "metallb.io/v1beta1"
-kind = "L2Advertisement"
-metadata = {
-  name = "generic-cluster-pool"
-  namespace = kubernetes_namespace.metallb.metadata[0].name
-}
-  YAML
+apiVersion: metallb.io/v1beta1
+kind: L2Advertisement
+metadata:
+  name: generic-cluster-pool
+  namespace: ${kubernetes_namespace.metallb.metadata[0].name}
+YAML
 }
