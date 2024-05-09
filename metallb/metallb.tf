bd857e80a266432947b417726669563bbf1686b6
steled <steled@gmx.net>
2024-05-09 07:07:54 +0200

    registry update

--- metallb/metallb.tf
+++ metallb/metallb.tf
@@ -8,9 +8,9 @@ resource "helm_release" "metallb" {
   name          = "metallb"
   namespace     = kubernetes_namespace.metallb.metadata[0].name
 
-  repository    = "https://charts.bitnami.com/bitnami"
+  repository    = "oci://registry-1.docker.io/bitnamicharts/metallb"
   chart         = "metallb"
-  version       = var.metallb_version # check version here: https://github.com/bitnami/charts/blob/master/bitnami/metallb/Chart.yaml
+  version       = var.metallb_version # check version here: https://github.com/bitnami/charts/blob/main/bitnami/metallb/Chart.yaml#L38
   recreate_pods = true
 
   values = [ file(var.values_yaml) ]
