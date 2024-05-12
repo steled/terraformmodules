2df3e3ca733644609f062dc67b618ea22f30aac6
steled <steled@gmx.net>
2024-05-12 14:01:21 +0200

    test

--- metallb/config.tf
+++ metallb/config.tf
@@ -13,14 +13,14 @@ YAML
   depends_on = [ helm_release.metallb ]
 }
 
-resource "kubectl_manifest" "metallb_l2advertisement" {
-  yaml_body = <<YAML
-apiVersion: metallb.io/v1beta1
-kind: L2Advertisement
-metadata:
-  name: generic-cluster-pool
-  namespace: ${kubernetes_namespace.metallb.metadata[0].name}
-YAML
+# resource "kubectl_manifest" "metallb_l2advertisement" {
+#   yaml_body = <<YAML
+# apiVersion: metallb.io/v1beta1
+# kind: L2Advertisement
+# metadata:
+#   name: generic-cluster-pool
+#   namespace: ${kubernetes_namespace.metallb.metadata[0].name}
+# YAML
 
-  depends_on = [ helm_release.metallb ]
-}
+#   depends_on = [ helm_release.metallb ]
+# }
