762633223bf69843800e1465b06848aaf7f11c01
steled <steled@gmx.net>
2024-07-27 05:56:14 +0200

    updating helm release

--- cert-manager/duckdns-webhook.tf
+++ cert-manager/duckdns-webhook.tf
@@ -2,9 +2,9 @@ resource "helm_release" "duckdns_webhook" {
   name       = "duckdns-webhook"
   namespace  = kubernetes_namespace.certmanager.metadata[0].name
 
-  repository = "https://ebrianne.github.io/helm-charts"
+  repository = "https://steled.github.io/cert-manager-webhook-duckdns/"
   chart      = "cert-manager-webhook-duckdns"
-  version    = var.duckdns_webhook_version # check version here: https://github.com/ebrianne/helm-charts/blob/master/charts/cert-manager-webhook-duckdns/Chart.yaml
+  version    = var.duckdns_webhook_version # check version here: https://github.com/steled/cert-manager-webhook-duckdns/blob/master/charts/cert-manager-webhook-duckdns/Chart.yaml#L4
 
   values = [ file(var.duckdns_webhook_values_yaml) ]
 }
