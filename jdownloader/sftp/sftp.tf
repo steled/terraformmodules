56b7db35e7881ab2afebc1e68717b0489e209f37
steled <steled@gmx.net>
2024-10-04 06:45:52 +0200

    updating version

--- jdownloader/sftp/sftp.tf
+++ jdownloader/sftp/sftp.tf
@@ -2,7 +2,7 @@ resource "helm_release" "sftp" {
   name      = "sftp"
   namespace = var.namespace
 
-  chart = "https://github.com/steled/sftp-server/archive/refs/tags/v0.3.5.tar.gz"
+  chart = "https://github.com/steled/sftp-server/archive/refs/tags/v0.3.11.tar.gz"
 
   values = [ file(var.values_yaml) ]
 }
