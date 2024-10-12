98152c6246daae23fafef36e10a7be1da3f73999
steled <steled@gmx.net>
2024-10-12 05:45:20 +0200

    chart name fix & adding ip address variabel

--- cert-manager/duckdns-webhook.tf
+++ cert-manager/duckdns-webhook.tf
@@ -1,10 +1,14 @@
 resource "helm_release" "duckdns_webhook" {
-  name       = "duckdns-webhook"
+  name       = "cert-manager-webhook-duckdns"
   namespace  = kubernetes_namespace.certmanager.metadata[0].name
 
   repository = "https://steled.github.io/cert-manager-webhook-duckdns/"
   chart      = "cert-manager-webhook-duckdns"
   version    = var.duckdns_webhook_version # check version here: https://github.com/steled/cert-manager-webhook-duckdns/blob/master/charts/cert-manager-webhook-duckdns/Chart.yaml#L4
 
-  values = [ file(var.duckdns_webhook_values_yaml) ]
+  values = [ templatefile(var.duckdns_webhook_values_yaml, {
+    duckdns_webhook_ip_address = var.duckdns_webhook_ip_address
+  })]
+
+  depends_on = [ helm_release.certmanager ]
 }
