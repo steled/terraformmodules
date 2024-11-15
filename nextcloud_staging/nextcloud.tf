acaba067a9c24b2ed57833e9a8bb8a3202df090a
steled <steled@gmx.net>
2024-11-15 09:52:52 +0100

    update

--- nextcloud_staging/nextcloud.tf
+++ nextcloud_staging/nextcloud.tf
@@ -11,6 +11,10 @@ resource "kubernetes_namespace" "nextcloud_staging" {
 
   provisioner "remote-exec" {
     inline = [
+      "sudo mkdir --mode 0755 -p /ext/backup/nextcloud-staging/postgresql",
+      "sudo chown steled:steled -R /ext/backup/nextcloud-staging/postgresql/",
+      "sudo mkdir --mode 0755 -p /ext/backup/nextcloud-staging/server",
+      "sudo chown steled:steled -R /ext/backup/nextcloud-staging/server/",
       "sudo mkdir --mode 0755 -p /ext/persistent/nextcloud-staging/backup",
       "sudo chown 1001:1001 -R /ext/persistent/nextcloud-staging/backup/",
       "sudo mkdir --mode 0755 -p /ext/persistent/nextcloud-staging/server",
@@ -23,10 +27,20 @@ resource "kubernetes_namespace" "nextcloud_staging" {
       "sudo chown www-data:www-data -R /ext/persistent/nextcloud-staging/server/data/",
       "sudo mkdir --mode 0755 -p /ext/persistent/nextcloud-staging/server/html",
       "sudo chown www-data:www-data -R /ext/persistent/nextcloud-staging/server/html/",
+      "sudo mkdir --mode 0755 -p /ext/persistent/nextcloud-staging/server/root",
+      "sudo chown www-data:www-data -R /ext/persistent/nextcloud-staging/server/root/",
       "sudo mkdir --mode 0755 -p /ext/persistent/nextcloud-staging/server/themes",
       "sudo chown www-data:www-data -R /ext/persistent/nextcloud-staging/server/themes/",
+      "sudo mkdir --mode 0755 -p /ext/persistent/nextcloud-staging/server/tmp",
+      "sudo chown www-data:www-data -R /ext/persistent/nextcloud-staging/server/tmp/",
       "sudo mkdir --mode 0755 -p /ext/persistent/nextcloud-staging/postgresql",
       "sudo chown 1001:1001 -R /ext/persistent/nextcloud-staging/postgresql/",
+      "sudo mkdir --mode 0755 -p /ext/persistent/nextcloud-staging/redis",
+      "sudo chown 1001:1001 -R /ext/persistent/nextcloud-staging/redis/",
+      "sudo mkdir --mode 0755 -p /ext/persistent/nextcloud-staging/redis/master",
+      "sudo chown 1001:1001 -R /ext/persistent/nextcloud-staging/redis/master/",
+      "sudo mkdir --mode 0755 -p /ext/persistent/nextcloud-staging/redis/replica",
+      "sudo chown 1001:1001 -R /ext/persistent/nextcloud-staging/redis/replica/"
     ]
   }
 }
@@ -40,19 +54,20 @@ resource "helm_release" "nextcloud_staging" {
   recreate_pods = true
 
   values = [ templatefile("${path.module}/values.yaml", {
-    nextcloud_domain = var.nextcloud_domain,
-    environment = var.environment,
-    ip_address = var.ip_address,
-    nextcloud_proxies = var.nextcloud_proxies
-    mail_fromaddress = var.mail_fromaddress
-    mail_domain = var.mail_domain
-    smtp_host = var.smtp_host
-    smtp_port = var.smtp_port
-    smtp_username = var.smtp_username
-    smtp_password = var.smtp_password
-    postgresql_postgresqlusername = var. postgresql_postgresqlusername,
-    postgresql_postgresqlpassword = var.postgresql_postgresqlpassword,
-    postgresql_postgresqldatabase = var.postgresql_postgresqldatabase
+    nextcloud_domain    = var.nextcloud_domain,
+    environment         = var.environment,
+    ip_address          = var.ip_address,
+    nextcloud_proxies   = var.nextcloud_proxies
+    mail_fromaddress    = var.mail_fromaddress
+    mail_domain         = var.mail_domain
+    redis_password      = var.redis_password
+    smtp_host           = var.smtp_host
+    smtp_port           = var.smtp_port
+    smtp_username       = var.smtp_username
+    smtp_password       = var.smtp_password
+    postgresql_username = var.postgresql_username,
+    postgresql_password = var.postgresql_password,
+    postgresql_database = var.postgresql_database
   }) ]
 
   namespace = kubernetes_namespace.nextcloud_staging.metadata[0].name
@@ -63,6 +78,8 @@ resource "helm_release" "nextcloud_staging" {
     kubernetes_persistent_volume_claim.nextcloud_staging_server_pvc,
     kubernetes_persistent_volume_claim.nextcloud_staging_postgresql_pvc,
     kubernetes_persistent_volume_claim.nextcloud_staging_backup_pvc,
+    kubernetes_persistent_volume_claim.nextcloud_staging_redis_master_pvc,
+    kubernetes_persistent_volume_claim.nextcloud_staging_redis_replica_pvc,
     kubernetes_secret.nextcloud_staging_secret
   ]
 }
--- nextcloud_staging/secret.tf
+++ nextcloud_staging/secret.tf
@@ -7,6 +7,10 @@ resource "kubernetes_secret" "nextcloud_staging_secret" {
   data = {
     nextcloud-username = var.nextcloud_admin_username
     nextcloud-password = var.nextcloud_admin_password
+    smtp-host          = var.smtp_host
+    smtp-username      = var.smtp_username
+    smtp-password      = var.smtp_password
+    redis-password     = var.redis_password
   }
 
   type = "Opaque"
