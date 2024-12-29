f61404e23867e2e5d0e10532995d57b74dd0d588
steled <steled@gmx.net>
2024-12-29 15:27:39 +0100

    variable naming update

--- jdownloader/sftp/sftp.tf
+++ jdownloader/sftp/sftp.tf
@@ -2,7 +2,7 @@ resource "helm_release" "sftp" {
   name      = "sftp"
   namespace = var.namespace
 
-  chart = var.sftp_version # check version here: https://github.com/steled/sftp-server/tags
+  chart = var.sftp_chart # check version here: https://github.com/steled/sftp-server/tags
 
   values = [templatefile(var.values_yaml, {
     sftp_ip_address = var.sftp_ip_address
