2f9d381eaa159949c74ac75ef966633f75f0962b
steled <steled@gmx.net>
2025-02-16 11:15:43 +0100

    add label for argocd

--- cert-manager_cloudflare/secret.tf
+++ cert-manager_cloudflare/secret.tf
@@ -2,6 +2,9 @@ resource "kubernetes_secret" "cloudflare_api_token_secret" {
   metadata {
     name      = "cloudflare-api-token"
     namespace = kubernetes_namespace.certmanager.metadata[0].name
+    labels = {
+      "argocd.argoproj.io/instance" = "helm-cert-manager"
+    }
   }
 
   data = {
