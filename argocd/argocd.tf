57a8635d4bf72d17e6faf961285cf6be9d0b05f6
steled <steled@gmx.net>
2025-12-05 09:28:06 +0100

    [argocd] typo fix

--- argocd/argocd.tf
+++ argocd/argocd.tf
@@ -10,7 +10,7 @@ resource "kubernetes_namespace_v1" "argocd" {
 
 resource "helm_release" "argocd" {
   name      = "argocd"
-  namespace = kubernetes_namespace.argocd.metadata[0].name
+  namespace = kubernetes_namespace_v1.argocd.metadata[0].name
 
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
