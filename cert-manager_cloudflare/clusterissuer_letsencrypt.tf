ed00939470ed04ed5ffbd5721c62a4a5fa84ddf4
steled <steled@gmx.net>
2025-12-06 06:55:13 +0100

    [cert-manager] migrating to kubernetes_manifest #99

--- cert-manager_cloudflare/clusterissuer_letsencrypt.tf
+++ cert-manager_cloudflare/clusterissuer_letsencrypt.tf
@@ -1,22 +1,58 @@
-resource "kubectl_manifest" "cert_manager_cluster_issuer_letsencrypt_staging" {
-  yaml_body = <<YAML
-apiVersion: cert-manager.io/v1
-kind: ClusterIssuer
-metadata:
-  name: cloudflare-letsencrypt-staging
-spec:
-  acme:
-    email: ${var.cert_manager_email}
-    privateKeySecretRef:
-      name: cloudflare-letsencrypt-staging
-    server: https://acme-staging-v02.api.letsencrypt.org/directory
-    solvers:
-    - dns01:
-        cloudflare:
-          apiTokenSecretRef:
-            key: api-token
-            name: cloudflare-api-token
-YAML
+# resource "kubectl_manifest" "cert_manager_cluster_issuer_letsencrypt_staging" {
+#   yaml_body = <<YAML
+# apiVersion: cert-manager.io/v1
+# kind: ClusterIssuer
+# metadata:
+#   name: cloudflare-letsencrypt-staging
+# spec:
+#   acme:
+#     email: ${var.cert_manager_email}
+#     privateKeySecretRef:
+#       name: cloudflare-letsencrypt-staging
+#     server: https://acme-staging-v02.api.letsencrypt.org/directory
+#     solvers:
+#     - dns01:
+#         cloudflare:
+#           apiTokenSecretRef:
+#             key: api-token
+#             name: cloudflare-api-token
+# YAML
+
+#   depends_on = [
+#     kubernetes_secret_v1.cloudflare_api_token_secret,
+#     helm_release.certmanager
+#   ]
+# }
+
+resource "kubernetes_manifest" "cert_manager_cluster_issuer_letsencrypt_staging" {
+  manifest = {
+    "apiVersion" = "cert-manager.io/v1"
+    "kind"       = "ClusterIssuer"
+    "metadata" = {
+      "name" = "cloudflare-letsencrypt-staging"
+    }
+    "spec" = {
+      "acme" = {
+        "email" = var.cert_manager_email
+        "privateKeySecretRef" = {
+          "name" = "cloudflare-letsencrypt-staging"
+        }
+        "server" = "https://acme-staging-v02.api.letsencrypt.org/directory"
+        "solvers" = [
+          {
+            "dns01" = {
+              "cloudflare" = {
+                "apiTokenSecretRef" = {
+                  "key"  = "api-token"
+                  "name" = "cloudflare-api-token"
+                }
+              }
+            }
+          }
+        ]
+      }
+    }
+  }
 
   depends_on = [
     kubernetes_secret_v1.cloudflare_api_token_secret,
@@ -24,25 +60,61 @@ YAML
   ]
 }
 
-resource "kubectl_manifest" "cert_manager_cluster_issuer_letsencrypt_production" {
-  yaml_body = <<YAML
-apiVersion: cert-manager.io/v1
-kind: ClusterIssuer
-metadata:
-  name: cloudflare-letsencrypt-production
-spec:
-  acme:
-    email: ${var.cert_manager_email}
-    privateKeySecretRef:
-      name: cloudflare-letsencrypt-production
-    server: https://acme-v02.api.letsencrypt.org/directory
-    solvers:
-    - dns01:
-        cloudflare:
-          apiTokenSecretRef:
-            key: api-token
-            name: cloudflare-api-token
-YAML
+# resource "kubectl_manifest" "cert_manager_cluster_issuer_letsencrypt_production" {
+#   yaml_body = <<YAML
+# apiVersion: cert-manager.io/v1
+# kind: ClusterIssuer
+# metadata:
+#   name: cloudflare-letsencrypt-production
+# spec:
+#   acme:
+#     email: ${var.cert_manager_email}
+#     privateKeySecretRef:
+#       name: cloudflare-letsencrypt-production
+#     server: https://acme-v02.api.letsencrypt.org/directory
+#     solvers:
+#     - dns01:
+#         cloudflare:
+#           apiTokenSecretRef:
+#             key: api-token
+#             name: cloudflare-api-token
+# YAML
+
+#   depends_on = [
+#     kubernetes_secret_v1.cloudflare_api_token_secret,
+#     helm_release.certmanager
+#   ]
+# }
+
+resource "kubernetes_manifest" "cert_manager_cluster_issuer_letsencrypt_production" {
+  manifest = {
+    "apiVersion" = "cert-manager.io/v1"
+    "kind"       = "ClusterIssuer"
+    "metadata" = {
+      "name" = "cloudflare-letsencrypt-production"
+    }
+    "spec" = {
+      "acme" = {
+        "email" = var.cert_manager_email
+        "privateKeySecretRef" = {
+          "name" = "cloudflare-letsencrypt-production"
+        }
+        "server" = "https://acme-v02.api.letsencrypt.org/directory"
+        "solvers" = [
+          {
+            "dns01" = {
+              "cloudflare" = {
+                "apiTokenSecretRef" = {
+                  "key"  = "api-token"
+                  "name" = "cloudflare-api-token"
+                }
+              }
+            }
+          }
+        ]
+      }
+    }
+  }
 
   depends_on = [
     kubernetes_secret_v1.cloudflare_api_token_secret,
--- cert-manager_cloudflare/clusterissuer_selfsigned.tf
+++ cert-manager_cloudflare/clusterissuer_selfsigned.tf
@@ -1,58 +1,126 @@
-resource "kubectl_manifest" "cert_manager_cluster_issuer_selfsigned_issuer" {
-  yaml_body = <<YAML
-apiVersion: cert-manager.io/v1
-kind: ClusterIssuer
-metadata:
-  name: selfsigned-issuer
-spec:
-  selfSigned: {}
-YAML
+# resource "kubectl_manifest" "cert_manager_cluster_issuer_selfsigned_issuer" {
+#   yaml_body = <<YAML
+# apiVersion: cert-manager.io/v1
+# kind: ClusterIssuer
+# metadata:
+#   name: selfsigned-issuer
+# spec:
+#   selfSigned: {}
+# YAML
+
+#   depends_on = [helm_release.certmanager]
+# }
+
+resource "kubernetes_manifest" "cert_manager_cluster_issuer_selfsigned_issuer" {
+  manifest = {
+    "apiVersion" = "cert-manager.io/v1"
+    "kind"       = "ClusterIssuer"
+    "metadata" = {
+      "name" = "selfsigned-issuer"
+    }
+    "spec" = {
+      "selfSigned" = {}
+    }
+  }
 
   depends_on = [helm_release.certmanager]
 }
 
-resource "kubectl_manifest" "cert_manager_certificate_steled_selfsigned_ca" {
-  yaml_body = <<YAML
-apiVersion: cert-manager.io/v1
-kind: Certificate
-metadata:
-  name: steled-selfsigned-ca
-  namespace: ${kubernetes_namespace_v1.certmanager.metadata[0].name}
-spec:
-  commonName: steled-selfsigned-ca
-  isCA: true
-  issuerRef:
-    name: selfsigned-issuer
-    kind: ClusterIssuer
-    group: cert-manager.io
-  privateKey:
-    algorithm: ECDSA
-    size: 256
-  secretName: root-secret
-  subject:
-    organizations:
-      - steled
-YAML
+# resource "kubectl_manifest" "cert_manager_certificate_steled_selfsigned_ca" {
+#   yaml_body = <<YAML
+# apiVersion: cert-manager.io/v1
+# kind: Certificate
+# metadata:
+#   name: steled-selfsigned-ca
+#   namespace: ${kubernetes_namespace_v1.certmanager.metadata[0].name}
+# spec:
+#   commonName: steled-selfsigned-ca
+#   isCA: true
+#   issuerRef:
+#     name: selfsigned-issuer
+#     kind: ClusterIssuer
+#     group: cert-manager.io
+#   privateKey:
+#     algorithm: ECDSA
+#     size: 256
+#   secretName: root-secret
+#   subject:
+#     organizations:
+#       - steled
+# YAML
+
+#   depends_on = [
+#     helm_release.certmanager,
+#     kubectl_manifest.cert_manager_cluster_issuer_selfsigned_issuer
+#   ]
+# }
+
+resource "kubernetes_manifest" "cert_manager_certificate_steled_selfsigned_ca" {
+  manifest = {
+    "apiVersion" = "cert-manager.io/v1"
+    "kind"       = "Certificate"
+    "metadata" = {
+      "name"      = "steled-selfsigned-ca"
+      "namespace" = kubernetes_namespace_v1.certmanager.metadata[0].name
+    }
+    "spec" = {
+      "commonName" = "steled-selfsigned-ca"
+      "isCA"       = true
+      "issuerRef" = {
+        "name"  = "selfsigned-issuer"
+        "kind"  = "ClusterIssuer"
+        "group" = "cert-manager.io"
+      }
+      "privateKey" = {
+        "algorithm" = "ECDSA"
+        "size"      = 256
+      }
+      "secretName" = "root-secret"
+      "subject" = {
+        "organizations" = ["steled"]
+      }
+    }
+  }
 
   depends_on = [
     helm_release.certmanager,
-    kubectl_manifest.cert_manager_cluster_issuer_selfsigned_issuer
+    kubernetes_manifest.cert_manager_cluster_issuer_selfsigned_issuer
   ]
 }
 
-resource "kubectl_manifest" "cert_manager_cluster_issuer_steled_ca_issuer" {
-  yaml_body = <<YAML
-apiVersion: cert-manager.io/v1
-kind: ClusterIssuer
-metadata:
-  name: steled-ca-issuer
-spec:
-  ca:
-    secretName: root-secret
-YAML
+# resource "kubectl_manifest" "cert_manager_cluster_issuer_steled_ca_issuer" {
+#   yaml_body = <<YAML
+# apiVersion: cert-manager.io/v1
+# kind: ClusterIssuer
+# metadata:
+#   name: steled-ca-issuer
+# spec:
+#   ca:
+#     secretName: root-secret
+# YAML
+
+#   depends_on = [
+#     helm_release.certmanager,
+#     kubectl_manifest.cert_manager_cluster_issuer_selfsigned_issuer
+#   ]
+# }
+
+resource "kubernetes_manifest" "cert_manager_cluster_issuer_steled_ca_issuer" {
+  manifest = {
+    "apiVersion" = "cert-manager.io/v1"
+    "kind"       = "ClusterIssuer"
+    "metadata" = {
+      "name" = "steled-ca-issuer"
+    }
+    "spec" = {
+      "ca" = {
+        "secretName" = "root-secret"
+      }
+    }
+  }
 
   depends_on = [
     helm_release.certmanager,
-    kubectl_manifest.cert_manager_cluster_issuer_selfsigned_issuer
+    kubernetes_manifest.cert_manager_cluster_issuer_selfsigned_issuer
   ]
 }
