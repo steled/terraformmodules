de92e5f93c4b578406325b64576890a7234e4faf
steled <steled@gmx.net>
2025-12-06 06:54:35 +0100

    [metallb] migrating to kubernetes_manifest #99

--- metallb/config.tf
+++ metallb/config.tf
@@ -1,16 +1,30 @@
-resource "kubectl_manifest" "metallb_ipaddresspool" {
-  yaml_body = <<YAML
-apiVersion: metallb.io/v1beta1
-kind: IPAddressPool
-metadata:
-  name: generic-cluster-pool
-  namespace: ${kubernetes_namespace_v1.metallb.metadata[0].name}
-spec:
-  addresses:
-    ${var.helm_values_addresses}
-YAML
+# resource "kubectl_manifest" "metallb_ipaddresspool" {
+#   yaml_body = <<YAML
+# apiVersion: metallb.io/v1beta1
+# kind: IPAddressPool
+# metadata:
+#   name: generic-cluster-pool
+#   namespace: ${kubernetes_namespace_v1.metallb.metadata[0].name}
+# spec:
+#   addresses:
+#     ${var.helm_values_addresses}
+# YAML
 
-  depends_on = [helm_release.metallb]
+#   depends_on = [helm_release.metallb]
+# }
+
+resource "kubernetes_manifest" "metallb_ipaddresspool" {
+  manifest = {
+    "apiVersion" = "metallb.io/v1beta1"
+    "kind"       = "IPAddressPool"
+    "metadata" = {
+      "name"      = "generic-cluster-pool"
+      "namespace" = kubernetes_namespace_v1.metallb.metadata[0].name
+    }
+    "spec" = {
+      "addresses" = var.helm_values_addresses
+    }
+  }
 }
 
 # resource "kubectl_manifest" "metallb_l2advertisement" {
